c85efe9fe9c36a4639a4bcc7832e3cad4bb68882
Ptolemy <ptolemybarnes@gmail.com>
2015-04-28 11:35:14 +0100

    Passed test: it can 'FizzBuzz' when passed number divisible by both 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,5 +8,9 @@ describe 'fizzbuzz' do
   it 'returns "Buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'Buzz'
   end
+
+  it 'returns "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
 end
 
