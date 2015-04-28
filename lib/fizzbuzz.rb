c93e4057851290b075da54f03651c45cd1081fea
Ptolemy <ptolemybarnes@gmail.com>
2015-04-28 11:34:14 +0100

    Passed test: that it can "Buzz" when passed a number divisible by 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,6 @@
 def fizzbuzz(number)
   return 'Fizz' if number % 3 == 0 
+  return 'Buzz' if number % 5 == 0
 end
+
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,5 +4,9 @@ describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'Fizz' 
   end
+
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
 end
 
