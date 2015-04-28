ba933b8849c7d4d2e5662ea9a574fedc40efb920
Ptolemy <ptolemybarnes@gmail.com>
2015-04-28 11:42:30 +0100

    Refactored to move conditional logic to separate descriptive methods

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,19 @@
 def fizzbuzz(number)
-  return 'FizzBuzz' if number % 15 == 0
-  return 'Fizz' if number % 3 == 0 
-  return 'Buzz' if number % 5 == 0
+  return 'FizzBuzz' if divisible_by_fifteen?(number)
+  return 'Fizz' if divisible_by_three?(number) 
+  return 'Buzz' if divisible_by_five?(number) 
   number
 end
 
+def divisible_by_three?(number)
+  number % 3 == 0
+end
+
+def divisible_by_five?(number)
+  number % 5 == 0
+end
+
+def divisible_by_fifteen?(number)
+  number % 15 == 0
+end
 
