bf1740253486db73cc9b75db59a706f8f1032179
Ptolemy <ptolemybarnes@gmail.com>
2015-04-28 11:36:25 +0100

    Passed test: returns number when number is divisible by neither 3 nor 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,7 @@ def fizzbuzz(number)
   return 'FizzBuzz' if number % 15 == 0
   return 'Fizz' if number % 3 == 0 
   return 'Buzz' if number % 5 == 0
+  number
 end
 
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,5 +12,9 @@ describe 'fizzbuzz' do
   it 'returns "FizzBuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'FizzBuzz'
   end
+
+  it 'returns the number when it is divisible by neither 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
 
