d6a841683be926ae224dae8ac286d8b8437f4c19
Ptolemy <ptolemybarnes@gmail.com>
2015-04-28 11:31:28 +0100

    Passed first test: that it can 'Fizz'

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,4 @@
+def fizzbuzz(number)
+  'Fizz'
+end
+
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,8 @@
+require_relative '../lib/fizzbuzz.rb'
+
 describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 3
+    expect(fizzbuzz(3)).to eq 'Fizz' 
   end
 end
 
